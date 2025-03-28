5978130972f5c5739a67a289adc56c116b7b7bcb
Andreas Gilbert Leite <anlea016@osloskolen.no>
2025-03-28 10:38:15 +0100

    fix multiple temps per day

--- svalbard.py
+++ svalbard.py
@@ -2,33 +2,51 @@ import pandas as pd
 import math
 import matplotlib.pyplot as plt
 
+# Last inn Excel-fil
 xl_file = pd.ExcelFile("Data.xlsx")
-
 dfs = xl_file.parse('Sheet1')
 
+# Konverter kolonner til lister
 time_list = dfs['Time'].tolist()
 temperature_list = dfs['Temperatur (°C)'].tolist()
 skydekke_list = dfs['Skydekke (okta)'].tolist()
+dag_list = dfs['Dato'].tolist()
+måned_list = dfs['Måned'].tolist()
 
-print("Time List:")
-print(time_list)
+# Sett for å holde styr på hvilke datoer vi allerede har lagret
+sett_av_dager = set()
 
-print("Temperature List:")
-print(temperature_list)
-
+# Nye lister med kun én oppføring per dag per måned
 new_temp_list = []
 new_time_list = []
 new_skydekke_list = []
+new_dag_list = []
+new_måned_list = []
 
-for time, temp, sky in zip(time_list, temperature_list, skydekke_list):
-    if temp == temp and sky == sky: # if temp and skydekke != nan
-        new_temp_list.append(temp)
-        new_time_list.append(time)
-        new_skydekke_list.append(sky)
+# Iterer gjennom dataen
+for time, temp, sky, dato, måned in zip(time_list, temperature_list, skydekke_list, dag_list, måned_list):
+    # Sjekk at verdiene ikke er NaN
+    if not math.isnan(temp) and not math.isnan(sky):
+        # Sjekk at vi ikke allerede har lagt til denne datoen i denne måneden
+        if (måned, dato) not in sett_av_dager:
+            sett_av_dager.add((måned, dato))  # Lagre datoen som brukt
+            new_temp_list.append(temp)
+            new_time_list.append(time)
+            new_skydekke_list.append(sky)
+            new_dag_list.append(dato)
+            new_måned_list.append(måned)
 
+# Skriv ut resultatene
 print("Time List:")
 print(new_time_list)
 print("Temperature List:")
 print(new_temp_list)
 print('Skydekke List:')
 print(new_skydekke_list)
+print('Dag List:')
+print(new_dag_list)
+print('Måned List:')
+print(new_måned_list)
+
+print(len(new_temp_list))
+print(len(new_dag_list))
