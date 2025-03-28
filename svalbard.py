ccc3df84a2c4e09e225b4001bb15fe5bf22aa6d2
allsers <mail@aleksanderekman.no>
2025-03-28 09:48:33 +0100

    Update svalbard.py

--- svalbard.py
+++ svalbard.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 xl_file = pd.ExcelFile("Data.xlsx")
 
@@ -6,9 +7,13 @@ dfs = xl_file.parse('Sheet1')
 
 time_list = dfs['Time'].tolist()
 temperature_list = dfs['Temperatur (°C)'].tolist()
+skydekke_list = dfs['Skydekke (okta)'].tolist()
 
 print("Time List:")
 print(time_list)
 
 print("Temperature List:")
 print(temperature_list)
+
+print('Skydekke List:')
+print(skydekke_list)
