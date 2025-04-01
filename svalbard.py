4cbf148025ee95f35675785d43a388e4e8bbe54b
Jonas64 <jonaslynnebakken@gmail.com>
2025-04-01 08:38:58 +0200

    Glidende gjennomsnitt

--- svalbard.py
+++ svalbard.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
+import numpy as np
+import pylab
 
 # Last inn Excel-fil
 xl_file = pd.ExcelFile("Data.xlsx")
@@ -50,3 +52,21 @@ print(new_måned_list)
 
 print(len(new_temp_list))
 print(len(new_dag_list))
+
+dager = []
+for i in range(len(new_temp_list)):
+    dager.append(i+1)
+
+k = 15
+glidende_avg = []
+for i in range(k, len(new_temp_list)-k):
+    glidende_avg.append(pylab.mean(new_temp_list[(i-k):(i+k)]))
+
+xpoints = np.array(dager)
+ypoints = np.array(new_temp_list)
+
+glidende_avg_x = xpoints[k:len(new_temp_list) - k]
+
+plt.plot(ypoints)
+plt.plot(glidende_avg_x, glidende_avg, "k")
+plt.show()
